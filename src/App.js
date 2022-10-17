42f0e185097994741d37cd06c67f45a9176cc209
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-17 23:50:54 +0200

    React is practised

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Card from './components/Card';
 import Counter from './components/Counter';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Animals from './components/Animals';
 
 function App() {
   const [details, setDetails] = useState({})
 
 const fetchDetails = async () => {
  const {data} = await axios.get("https://randomuser.me/api/");
- console.log(data);
+ //console.log(data);
  const details = data.results[0]
  setDetails(details)
  console.log(details);
@@ -24,6 +25,7 @@ useEffect(() => {
       <div className = "App" >
         <Counter/>
         <Card details = {details} />
+       <Animals />
         {/* <Card  imageUrl="https://images.pexels.com/photos/161077/prague-vencel-square-czech-republic-church-161077.jpeg?auto=compress&cs=tinysrgb&w=800" />
         {<Card name= "Sivakumar" imageUrl="https://images.pexels.com/photos/9651000/pexels-photo-9651000.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" />}
         */} </div>
---
+++ src/components/Animals.jsx
@@ -0,0 +1,21 @@
+
+
+function Animals() {
+  return (
+    <>
+    
+    <div className="card" style={{width: "18rem"}}>
+        <h1>Animals</h1>
+  <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWFsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default Animals
